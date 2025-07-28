84c1a2ab7cb87f2b2f5077a568b51dec06529f53
Pawan Singh <p.singh@s-bb.nl>
2025-07-28 10:24:33 +0200

    fixed the file issues for the front end so that the website looks normal again

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './css/style.css';
+import './css/navbar.css';
+import './css/hero.css';
+import './css/story.css';
+import './css/dining.css';
+import './css/news.css';
+import './css/footer.css';
--- src/main.js
+++ src/main.js
@@ -1,30 +1,9 @@
-import './css/style.css'
-import './css/navbar.css'
-import './css/hero.css'
-import './css/story.css'
-import './css/dining.css'
-import './css/news.css'
-import './css/footer.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
